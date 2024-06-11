b02fe01b84a263d9856699a4b1d53cb7033b12e9
Pietro <pietroramos5@gmail.com>
2024-06-11 19:59:07 -0300

    Remove arquivos de configuração da IDE e diretórios compilados, reorganiza estrutura de diretórios e adiciona .gitignore

--- systemPOO/src/dao/interfaces/StorageDAO.java
+++
@@ -1,9 +0,0 @@
-package dao.interfaces;
-
-import entities.Product;
-
-public interface StorageDAO {
-	void addProduct(Product product, Double quantity) throws Exception;
-	void removeProduct(Product product, Double quantity) throws Exception;
-    int checkProductQuantity(Product product) throws Exception;
-}
--- systemPOO/src/entities/enums/MovementType.java
+++
@@ -1,7 +0,0 @@
-package entities.enums;
-
-public enum MovementType {
-    PURCHASE,
-    SALE,
-    ADJUSTMENT
-}
